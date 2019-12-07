8867a96681daf5d2fe929511428c124b178f38c1
Brandon C. Brown <brandon.c.brown@outlook.com>
2019-12-06 22:42:47 -0500

    boostrap added to project

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return 'This is the worst!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
